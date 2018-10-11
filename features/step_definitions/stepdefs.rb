7f574acbe1fcb046ebc1a0dcbcdf907855489102
Bruce Becker <brucellino@gmail.com>
2018-10-11 10:08:29 +0200

    add assertion on idp proxy page

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -11,6 +11,7 @@ Then('I click on the red log in button on the top right') do
 end
 
 Then('I am taken to the IdP Proxy') do
+  expect(@browser.url).to match(/aai\.egi\.eu\/proxy\/module\.php\/discopower\/disco.php\?entityID=https%3A%2F%2Faai\.egi\.eu/)
 end
 
 Then('I can see the ORCID option to log in.') do
