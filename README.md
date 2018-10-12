1e0ec445e49cb54d0dbf9b30872f0788ddffaf51
Bruce Becker <brucellino@gmail.com>
2018-10-12 17:25:53 +0200

    Update readme and add failing test for orcid visibility

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Behaviour-Driven Development features for Check-In. Run the tests yourself in wt
 ## Features
 
 Features of Check-In that we test for are described in the `[features](features)` directory
+
+## Executing tests
+
+To execute tests, you need:
+
+  - ruby and bundler
+  - Cucumber
+  - selenium and gecko web driver
+
+Once you have those, just run `cucumber` if you want to see what happens, or `test.sh` if you want to send test coverage results to [cucumber.pro](https://app.cucumber.pro)
--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,9 +1,17 @@
 require 'cucumber'
 require 'watir'
 
+visbile_link_script = %q{
+  var rect = arguments[0].getBoundingClientRect(); 
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) );
+}
+
 Given('I try to log in to EGI Check-In') do
   @browser = Watir::Browser.new :firefox
-  @browser.window.resize_to 1600, 1200
   @browser.goto('https://aai.egi.eu')
 end
 
@@ -18,6 +26,8 @@ end
 Then('I can see the ORCID option to log in.') do
   @orcid_link = @browser.link(class: 'orcid', text: /orcid/i)
   expect(@orcid_link.present?).to be
+  @browser.div(id: 'loader').wait_while(&:present?)
+  expect(@browser.execute_script(visbile_link_script, @orcid_link)).to be_truthy
 end
 
 After do
